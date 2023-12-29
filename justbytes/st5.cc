0ccaea163e43cae930e260eacc6b96e8a2516e06
Priyam Tejaswin <priyamtejaswin@gmail.com>
2023-12-29 11:14:35 +0530

    Finished charsmap decoding.

--- justbytes/st5.cc
+++ justbytes/st5.cc
@@ -1,4 +1,5 @@
 #include "st5.h"
+#include "darts.h"
 #include <string>
 #include <iostream>
 #include <array>
@@ -9,6 +10,9 @@ Normalizer::Normalizer(std::string_view name) {
     // Load pre-compiled rules.
     std::string precompiled_chars_map;
     GetPrecompiledCharsMap(name, &precompiled_chars_map);
+
+    // Decompile to chars-map.
+    DecompileCharsMap(precompiled_chars_map, &chars_map);
 }
 
 Normalizer::~Normalizer() {}
@@ -21,7 +25,7 @@ void Normalizer::GetPrecompiledCharsMap(
         const auto *blob = &kNormalizationRules_blob[i];
         if (blob->name == name) {
             output->assign(blob->data, blob->size);
-            std::cout << "Found!" << std::endl;
+            std::cout << "Found " << name << std::endl;
         }
     }
 }
@@ -41,14 +45,53 @@ void Normalizer::DecodePrecompiledCharsMap(
     *normalized = std::string_view(blob.data(), blob.size());
 }
 
-// void Normalizer::DecompileCharsMap(
-//     std::string_view rules,
-//     std::string_view blob, 
-//     CharsMap *chars_map
-//     ) {
-//     chars_map->clear();
-//     std::string_view trie_blob, normalized;
-//     DecodePrecompiledCharsMap(rules, &trie_blob, &normalized);
-// }
+void Normalizer::DecompileCharsMap(
+    std::string_view blob, // View into the blob, not the blob itself!
+    CharsMap *chars_map
+    ) {
+    chars_map->clear();
+    
+    std::string_view trie_blob, normalized;
+    std::string buf;
+
+    DecodePrecompiledCharsMap(blob, &trie_blob, &normalized);
+    
+    Darts::DoubleArray trie;
+    trie.set_array(const_cast<char *>(trie_blob.data()),
+                   trie_blob.size() / trie.unit_size());
+    
+    std::string key;
+    std::function<void(size_t, size_t)> traverse;
+
+    // Given a Trie node at `node_pos` and the key position at `key_position`,
+    // Expands children nodes from `node_pos`.
+    // When leaf nodes are found, stores them into `chars_map`.
+    traverse = [&traverse, &key, &trie, &normalized, &chars_map](
+                    size_t node_pos, size_t key_pos) -> void {
+        for (int c = 0; c <= 255; ++c) {
+            key.push_back(static_cast<char>(c));
+            size_t copied_node_pos = node_pos;
+            size_t copied_key_pos = key_pos;
+            // Note: `copied_(node|key)_pos` are non-const references.
+            // They store the new positions after node traversal.
+            const Darts::DoubleArray::result_type result = trie.traverse(
+                key.data(), copied_node_pos, copied_key_pos, key.size());
+            if (result >= -1) {   // node exists.
+                if (result >= 0) {  // has a value after transition.
+                    const std::string_view value = normalized.data() + result;
+                    Chars key_chars, value_chars;
+                    for (const auto c : string_util::UTF8ToUnicodeText(key))
+                        key_chars.push_back(c);
+                    for (const auto c : string_util::UTF8ToUnicodeText(value))
+                        value_chars.push_back(c);
+                    (*chars_map)[key_chars] = value_chars;
+                }
+                // Recursively traverse.
+                traverse(copied_node_pos, copied_key_pos);
+            }
+            key.pop_back();
+        }
+    };
+}
 
 } // namespace st5
--- justbytes/st5.h
+++ justbytes/st5.h
@@ -25,10 +25,14 @@ using Chars = std::vector<char32>;
 // String-to-string mapping.
 using CharsMap = std::map<Chars, Chars>;
 
+// Storing unicode text.
+using UnicodeText = std::vector<char32>;
+
 class Normalizer {
     public:
     Normalizer(std::string_view name);
     ~Normalizer();
+    CharsMap chars_map;
 
     private:
     void GetPrecompiledCharsMap(
@@ -40,8 +44,13 @@ class Normalizer {
         std::string_view *trie_blob,
         std::string_view *normalized
     );
-    // void DecompileCharsMap(std::string_vi, std::string_view blob, CharsMap *chars_map);
+    void DecompileCharsMap(
+        std::string_view blob,
+        CharsMap *chars_map
+    );
 };
+
+namespace string_util { // Contains utility funtion definitions.
 
 template <typename T>
 inline bool DecodePOD(std::string_view str, T *result) {
@@ -51,6 +60,65 @@ inline bool DecodePOD(std::string_view str, T *result) {
     memcpy(result, str.data(), sizeof(T));
     return true;
 }
+
+UnicodeText UTF8ToUnicodeText(std::string_view utf8) {
+    UnicodeText uc;
+    const char *begin = utf8.data();
+    const char *end = utf8.data() + utf8.size();
+    while (begin < end) {
+        size_t mblen;
+        const char32 c = DecodeUTF8(begin, end, &mblen);
+        uc.push_back(c);
+        begin += mblen;
+    }
+    return uc;
+}
+
+// mblen sotres the number of bytes consumed after decoding.
+char32 DecodeUTF8(const char *begin, const char *end, size_t *mblen) {
+  const size_t len = end - begin;
+
+  if (static_cast<unsigned char>(begin[0]) < 0x80) {
+    *mblen = 1;
+    return static_cast<unsigned char>(begin[0]);
+  } else if (len >= 2 && (begin[0] & 0xE0) == 0xC0) {
+    const char32 cp = (((begin[0] & 0x1F) << 6) | ((begin[1] & 0x3F)));
+    if (IsTrailByte(begin[1]) && cp >= 0x0080 && IsValidCodepoint(cp)) {
+      *mblen = 2;
+      return cp;
+    }
+  } else if (len >= 3 && (begin[0] & 0xF0) == 0xE0) {
+    const char32 cp = (((begin[0] & 0x0F) << 12) | ((begin[1] & 0x3F) << 6) |
+                       ((begin[2] & 0x3F)));
+    if (IsTrailByte(begin[1]) && IsTrailByte(begin[2]) && cp >= 0x0800 &&
+        IsValidCodepoint(cp)) {
+      *mblen = 3;
+      return cp;
+    }
+  } else if (len >= 4 && (begin[0] & 0xf8) == 0xF0) {
+    const char32 cp = (((begin[0] & 0x07) << 18) | ((begin[1] & 0x3F) << 12) |
+                       ((begin[2] & 0x3F) << 6) | ((begin[3] & 0x3F)));
+    if (IsTrailByte(begin[1]) && IsTrailByte(begin[2]) &&
+        IsTrailByte(begin[3]) && cp >= 0x10000 && IsValidCodepoint(cp)) {
+      *mblen = 4;
+      return cp;
+    }
+  }
+
+  // Invalid UTF-8.
+  *mblen = 1;
+  return kUnicodeError;
+}
+
+// Return (x & 0xC0) == 0x80;
+// Since trail bytes are always in [0x80, 0xBF], we can optimize:
+inline bool IsTrailByte(char x) { return static_cast<signed char>(x) < -0x40; }
+
+inline bool IsValidCodepoint(char32 c) {
+  return (static_cast<uint32>(c) < 0xD800) || (c >= 0xE000 && c <= 0x10FFFF);
+}
+
+} // namespace string_util
 
 } // namespace st5
 
