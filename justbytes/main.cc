20e520eb06c60541d5d48d19a56ac8d2d1b6351b
Priyam Tejaswin <priyamtejaswin@gmail.com>
2023-12-31 09:10:56 +0530

    Works. But wrong.

--- justbytes/main.cc
+++ justbytes/main.cc
@@ -3,5 +3,9 @@
 
 int main(int argc, char *argv[]) {
     st5::Normalizer normalizer("nmt_nfkc");
+    std::string test_str = "  HELLO  ";
+    auto test_out = normalizer.Normalize(test_str);
+    std::cout << test_str << std::endl;
+    std::cout << test_out << std::endl;
     return 0;
 }
